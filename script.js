5f639b85038c24f64fcbe7ba3e100d249d0e11fb
Azam <103303519+Azam92053@users.noreply.github.com>
2026-01-20 01:18:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
             let radius = prompt("Enter the radius of the circle:"); 
             // write you code here and display the result to the user
 			let r = +radius;
-			let areaOfCircle = (22/7) * r * r;
+			let areaOfCircle = 3.14159 * r * r;
 			 alert("The area of the circle with radius " + r + " is " + areaOfCircle.toFixed(2));
            
 }
